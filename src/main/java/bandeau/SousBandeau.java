00c05cfcbbfbd47eefb4002b15e0b229b75a0694
alockfat <amelie.lock-fat@etud.univ-jfc.fr>
2023-10-04 17:42:58 +0200

    travail fini

--- src/main/java/bandeau/SousBandeau.java
+++ src/main/java/bandeau/SousBandeau.java
@@ -6,32 +6,15 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class SousBandeau extends Bandeau {
     private final Lock lock = new ReentrantLock();
-    private final Condition condition = lock.newCondition();
-    private static int sbCount = 0;
-    private boolean iAmFree = true;
-    private final int myNumber;
 
-    public SousBandeau() {
-        myNumber = ++sbCount;
+    public void verrouille(){
+        lock.lock();
     }
 
-    synchronized public void take() throws InterruptedException {
-        while (!iAmFree) {
-            wait();
-        }
-        // assert iAmFree;
-        iAmFree = false;
-        System.out.println("Bandeau " + myNumber + " Taken");
-
+    public void deverrouille(){
+        lock.unlock();
     }
 
-
-    synchronized public void release() {
-        // assert !iAmFree;
-        System.out.println("Bandeau " + myNumber + " Released");
-        iAmFree = true;
-        notifyAll();
-    }
 }
 
 
